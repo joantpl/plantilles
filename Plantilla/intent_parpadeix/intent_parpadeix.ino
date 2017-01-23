42c4554ae24e23e474b4462c5bed91465259f071
joantpl <jotopl13@gmail.com>
2017-01-23 13:55:19 +0100

    Pràctica 1 blink
    Ençesa i aturada de led cada mig segon

---
+++ Plantilla/intent_parpadeix/intent_parpadeix.ino
@@ -0,0 +1,14 @@
+int ledPin = 13;
+boolean flag = true;
+long previousMillis = 0;
+long On = 50;             // tiempo que dura encendido, en milisegundos
+long Off = 1000;          // tiempo que dura apagado, en milisegundos
+
+void setup(){
+ pinMode(ledPin, OUTPUT);
+}
+
+
+void loop()
+  
+ 
